162ee1749a14ed0d4fdf6d022b275d91dc980ff8
JeffAllison <itsjallison@gmail.com>
2022-02-28 15:10:50 -0800

    convertoseconds

--- app.js
+++ app.js
@@ -6,7 +6,9 @@
 // a number of minutes as an argument and returns
 // the number of seconds
 
-function convertToSeconds() {}
+function convertToSeconds(minutes){
+    return minutes * 60;
+}
 
 console.log(`convertToSeconds(10) === 600 : ${convertToSeconds(10) === 600}`);
 
@@ -19,12 +21,7 @@ console.log(`areaOfTriangle(5, 10) === 25 : ${areaOfTriangle(5, 10) === 25}`);
 // 3. lessThan100
 // Given 2 numbers, return true if their sum is less than 100
 // return false if it is greater than 100
-function lessThan100(number 1, number 2) {
-    if (number1 + number2 < 100) {
-        return true; 
-    } else if (number1 + number2 >= 100)
-    }
-}
+function lessThan100() {}
 console.log(`lessThan100(5, 10) === true : ${lessThan100(5, 10) === true}`);
 console.log(`lessThan100(100, 10) === false : ${lessThan100(100, 10) === false}`);
 
