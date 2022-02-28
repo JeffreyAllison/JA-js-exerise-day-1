bee084dfedfe211d8acd06a5caf56e270836258b
JeffAllison <itsjallison@gmail.com>
2022-02-28 15:25:55 -0800

    triangle

--- app.js
+++ app.js
@@ -9,13 +9,15 @@
 function convertToSeconds(minutes){
     return minutes * 60;
 }
-
 console.log(`convertToSeconds(10) === 600 : ${convertToSeconds(10) === 600}`);
 
 // 2. areaOfTriangle
 // Write a function called areaOfTriangle that takes in the base and height
 // of a triangle and returns its area
-function areaOfTriangle() {}
+function areaOfTriangle(base, height) {
+    return base + height + height; 
+}
+
 console.log(`areaOfTriangle(5, 10) === 25 : ${areaOfTriangle(5, 10) === 25}`);
 
 // 3. lessThan100
